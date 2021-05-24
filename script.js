196575a45df7f1e95c56241e325f2bdf6cce8663
Matías Ortiz <matiasortiz01@outlook.com>
2021-05-23 22:05:41 -0400

    Cambios visuales

---
+++ script.js
@@ -0,0 +1,8 @@
+const app = new Vue({
+    el:'#section',
+    data:{
+        templates: [{titulo:"Título", src:"img/MiradorSanguchería/header.jpg", descripcion:"descripcion"},
+                    ],
+    }
+    
+})
