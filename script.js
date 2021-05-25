03227597845be97615b0653c8b165c252cd121a1
Matías Ortiz <matiasortiz01@outlook.com>
2021-05-25 00:17:57 -0400

    Hotfix: arreglo de error en vista cat disaster (inglés)

--- script.js
+++
@@ -1,8 +0,0 @@
-const app = new Vue({
-    el:'#section',
-    data:{
-        templates: [{titulo:"Título", src:"img/MiradorSanguchería/header.jpg", descripcion:"descripcion"},
-                    ],
-    }
-    
-})
